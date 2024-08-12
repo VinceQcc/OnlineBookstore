d52df26845dc467e680e8f6eb3364f282291fca1
chopsticks-user <frostyfrost273@gmail.com>
2024-08-12 08:03:27 -0400

    Add docker compose to run server with database

--- server/src/main/kotlin/com/vinceqcc/bookstore/book/Book.kt
+++ server/src/main/kotlin/com/vinceqcc/bookstore/book/Book.kt
@@ -1,30 +1,13 @@
 package com.vinceqcc.bookstore.book
 
-import jakarta.persistence.*
-import lombok.AllArgsConstructor
-import lombok.Builder
-import lombok.NoArgsConstructor
-
-
-@Table(name = "books")
-@Entity
-data class Book(
-    @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(updatable = false, unique = true, nullable = false)
-    var id: String?,
-
-    @Column(unique = false)
-    var title: String = "",
-) {
-    constructor() : this(id = "", title = "")
-
-    class Builder {
-        private var id: String = "";
-        private var title: String = "";
-
-        fun id(id: String) = apply { this.id = id }
-        fun title(title: String) = apply { this.title = title }
-        fun build() = Book(id, title)
-    }
-}
+//@Table(name = "books")
+//@Entity
+//data class Book(
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.UUID)
+//    @Column(updatable = false, unique = true, nullable = false)
+//    var id: String? = null,
+//
+//    @Column(unique = false)
+//    var title: String,
+//)
