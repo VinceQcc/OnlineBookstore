1fb4f5348a71bad7314561aeeac013f10400f72d
chopsticks-user <frostyfrost273@gmail.com>
2024-08-12 08:26:44 -0400

    Book

--- server/src/main/kotlin/com/vinceqcc/bookstore/book/Book.kt
+++ server/src/main/kotlin/com/vinceqcc/bookstore/book/Book.kt
@@ -1,13 +1,25 @@
 package com.vinceqcc.bookstore.book
 
-//@Table(name = "books")
-//@Entity
-//data class Book(
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.UUID)
-//    @Column(updatable = false, unique = true, nullable = false)
-//    var id: String? = null,
-//
-//    @Column(unique = false)
-//    var title: String,
-//)
+import jakarta.persistence.*
+import org.springframework.data.repository.CrudRepository
+import java.time.LocalDate
+
+@Table(name = "books")
+@Entity
+data class Book(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(updatable = false, unique = true, nullable = false)
+    var id: String? = null,
+
+    val title: String,
+    val author: String,
+    val description: String? = null,
+    val price: Double,
+    val stockQuantity: Int? = 0,
+    val publishedDate: LocalDate,
+    val imageUrl: String? = null,
+    val genre: String? = null,
+)
+
+interface BookRepository : CrudRepository<Book, String> {}
